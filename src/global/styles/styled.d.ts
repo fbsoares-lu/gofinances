b044fb751fb3ed275a1d9b325cf8b5a7ea8613d1
fbsoares-lu <lsoareshfb@gmail.com>
2021-06-12 01:58:29 -0300

    first commit

---
+++ src/global/styles/styled.d.ts
@@ -0,0 +1,9 @@
+import 'styled-components';
+
+import theme from './theme';
+
+declare module 'styled-components' {
+    type themeType = typeof theme
+
+    export interface DefaultTheme extends themeType {}
+}
