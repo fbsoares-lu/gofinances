fc085b18c95fb1e6abecbb9f23927c2d2ec57cb9
fbsoares-lu <lsoareshfb@gmail.com>
2021-06-20 00:41:44 -0300

    Create new component to list all transactions

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import { TransactionCard, TransactionCardProps } from '../../components/TransactionCard';
 import { HighlightCard } from '../../components/HighlightCard';
 
 import { 
@@ -11,10 +13,53 @@ import {
     UserGreeting,
     UserName,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionsList,
 } from './styles';
 
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
+
 export function Dashboard() {
+    const data: DataListProps[] = [
+        {   
+            id: '1',
+            type: 'positive',
+            title: "Desenvolvimento de site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'Vendas',
+                icon: 'dollar-sign',
+            },
+            date: '13/04/2020'
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: "Hamburgueria Pizzy",
+            amount: "R$ 59,90",
+            category: {
+                name: 'Alimentação',
+                icon: 'coffee',
+            },
+            date: '10/04/2020'
+        },
+        {   
+            id: '3',
+            type: 'negative',
+            title: "Aluguel do apartamento",
+            amount: "R$ 1.200,00",
+            category: {
+                name: 'Casa',
+                icon: 'shopping-bag',
+            },
+            date: '27/03/2020'
+        },
+    ];
+
     return (
         <Container>
             <Header>
@@ -34,9 +79,7 @@ export function Dashboard() {
 
 
             </Header>
-            <HighlightCards
-                
-            >
+            <HighlightCards>
                 <HighlightCard
                     type="up"
                     title="Entradas"
@@ -56,6 +99,16 @@ export function Dashboard() {
                     lastTransaction="01 à 16 de abril"
                 />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionsList 
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item}/>}
+                />
+            </Transactions>
         </Container>
     );
 }
